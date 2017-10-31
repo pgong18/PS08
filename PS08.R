38e6146b22cf209e7e7d11a1997291ac5d13d06e
pei gong <pgong18@amherst.edu>
2017-10-30 22:02:02 -0400

    Pei's HW

--- PS08.R
+++ PS08.R
@@ -20,8 +20,8 @@ dim(train)
 model_formula <- as.formula(Device ~ X + Y + Z)
 
 # Values to use:
-n_values <- seq(from=1,to=2900000,by=2000)
-k_values <- seq(from=1, to=10000,by=20)
+n_values <- seq(from=1,to=2900000,by=20000)
+k_values <- seq(from=1, to=10000,by=2000)
 
 runtime_dataframe <- expand.grid(n_values, k_values) %>%
   as_tibble() %>%
@@ -52,14 +52,22 @@ write.csv(runtime_dataframe1,file="runtime.csv")
 # Think of creative ways to improve this barebones plot. Note: you don't have to
 # necessarily use geom_point
 
-ggplot(runtime_dataframe1) +
-  geom_point(aes(x=n, y=k, col=log10(runtime)), size=2)
+runtime_plot1<-ggplot(runtime_dataframe1) +
+  geom_point(aes(x=n, y=k, col=runtime), size=2)
+runtime_plot1
 runtime_plot2 <- ggplot(runtime_dataframe1, aes(x=n, y = runtime, group=k, col = k)) +
   geom_line() +
   labs(title = "Runtime with Varying n")
 runtime_plot2
+runtime_plot3 <- ggplot(runtime_dataframe1, aes(x=k, y = runtime, group=n, col = n)) +
+  geom_line() +
+  labs(title = "Runtime with Varying k")
+runtime_plot3
 
-runtime_plot
+runtime_plot4<-ggplot(runtime_dataframe1, aes(x=n, y=k))+
+  geom_raster(aes(fill = runtime), hjust=0.5, vjust=0.5, interpolate=FALSE)
+
+runtime_plot4
 ggsave(filename="Pei_Gong.png", width=16, height = 9)
 
 
@@ -74,8 +82,9 @@ ggsave(filename="Pei_Gong.png", width=16, height = 9)
 #step1, for each of the points we compute all the distance , which is nd 
 #step2: after storing the distance nd,we compare the values to find k neighbors, 
 #which involve loopoing through the distance, for n points we have complexity nk 
+#therefore in total we have O(nd+nk). when d=3, we have O(3n+nk). 
 
-#therefore in total we have O(nd+nk). when d=3, we have O(3n+nk)
+#The graph above supports my hypothesis. 
 
 
 
